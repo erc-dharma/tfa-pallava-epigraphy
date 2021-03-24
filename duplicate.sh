f578f30669cccaab5a97e1b19fa046624452a882
ajaniak <33328906+ajaniak@users.noreply.github.com>
2021-03-24 12:40:14 +0100

    trying to set up a quick and dirty github page for tfa-pallava

--- duplicate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-dirname=./texts/xml
-echo ::set-output name=duplicate_filename::$(
-find $dirname -type f | sed 's_.*/__' | sort|  uniq -d|
-while read fileName
-do
-find $dirname -type f | grep "$fileName"
-done
-)
